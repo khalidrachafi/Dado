5255fef6af41c96841b232e4c0c584cd90c7c486
kracelh542 <kracelh542@gmail.com>
2024-01-08 09:20:34 +0100

    Iniciamos proyecto

---
+++ src/main/java/daw/Dado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author khalid
+ */
+
+
+//constructor parametrizado con el número de tiradas a generar
+//toString que devuelva de cada elemento de la lista su posición y su valor
+//método para saber el número de veces que ha salido una cara
+//método para borrar todas las caras indicadas como parámetro
+public class Dado {
+
+    
+}
