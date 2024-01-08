45d9ac4e749e2e239329601204abba7f0ca219e4
kracelh542 <kracelh542@gmail.com>
2024-01-08 09:36:45 +0100

    empezamos proyecto con enunciado correcto

--- src/main/java/daw/Dado.java
+++ src/main/java/daw/Dado.java
@@ -16,6 +16,16 @@ import java.util.Random;
 //toString que devuelva de cada elemento de la lista su posición y su valor
 //método para saber el número de veces que ha salido una cara
 //método para borrar todas las caras indicadas como parámetro
+
+
+
+
+//Clase LanzarDado, con un atributo lista de enteros.
+//EL constructor de del objeto rellena una lista simulando 1000 lanzamientos del dado
+//constructor parametrizado con el número de tiradas a generar
+//toString que devuelva de cada elemento de la lista su posición y su valor
+//método para saber el número de veces que ha salido una cara
+//método para borrar todas las caras indicadas
 public class Dado {
 
     public int minCaras;
